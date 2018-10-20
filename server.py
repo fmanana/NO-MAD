e7cc80bcea492fdf2bc26dceaaf02f3a3f4eb6b4
fmanana <fezile.manana@protonmail.com>
2018-10-20 20:24:19 +0200

    Forgot dates.json

--- server.py
+++ server.py
@@ -21,6 +21,7 @@ def search():
     if request.method == 'POST':
         university = request.form['university']
         recommended = json.loads(recommend.uni_data(university))
+        print( json.dumps(recommended, indent=2))
         return render_template('flights.html', flights=recommended)
     else:
         return render_template('index.html')
