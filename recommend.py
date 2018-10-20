ec6d5e2be7b820a01fc2667ba6337af2925cff0d
Henok Seifu <hseifu@jacobs-university.de>
2018-10-20 20:18:50 +0200

    Airlines returned with their names

--- recommend.py
+++ recommend.py
@@ -48,13 +48,20 @@ def recommend(start, end, location):
     for place in quotes["Places"]:
         new_places[str(place["PlaceId"])] = place["Name"]
 
-    #Change ids to names for places and get date alone from time
+    #Carrier library. id:name
+    new_airlines = {}
+    for airline in quotes["Carriers"]:
+        new_airlines[str(airline["CarrierId"])] = airline["Name"]
+
+    #Change ids, to names for places and get date alone from time
     for quote in filtered_quotes["Quotes"]:
         quote["OutboundLeg"]["OriginId"] = new_places[str(quote["OutboundLeg"]["OriginId"])]
         quote["OutboundLeg"]["DestinationId"] = new_places[str(quote["OutboundLeg"]["DestinationId"])]
+        quote["OutboundLeg"]["CarrierIds"][0] = new_airlines[str(quote["OutboundLeg"]["CarrierIds"][0])]
         quote["OutboundLeg"]["DepartureDate"] = quote["OutboundLeg"]["DepartureDate"].split('T')[0]
         quote["InboundLeg"]["OriginId"] = new_places[str(quote["InboundLeg"]["OriginId"])]
         quote["InboundLeg"]["DestinationId"] = new_places[str(quote["InboundLeg"]["DestinationId"])]
+        quote["InboundLeg"]["CarrierIds"][0] = new_airlines[str(quote["InboundLeg"]["CarrierIds"][0])]
         quote["InboundLeg"]["DepartureDate"] = quote["InboundLeg"]["DepartureDate"].split('T')[0]
     #return filtered json
     return filtered_quotes
