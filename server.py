d3ecda8b2f3fbc3af6f2fda22398fca66fe6ac6a
fmanana <fezile.manana@protonmail.com>
2018-10-20 23:31:47 +0200

    Contact page

--- server.py
+++ server.py
@@ -21,14 +21,14 @@ def search():
     if request.method == 'POST':
         university = request.form['university']
         recommended = json.loads(recommend.uni_data(university))
-        print( json.dumps(recommended, indent=2))
+        #print( json.dumps(recommended, indent=2))
         return render_template('flights.html', flights=recommended)
     else:
         return render_template('index.html')
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    if request.method == 'POST':
+    if request.method == 'GET':
         return render_template('contact.html')
     else:
         return render_template('index.html')
